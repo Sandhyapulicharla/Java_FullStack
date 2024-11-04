1f0606f89d79e1c9b1bd42dced93b7e847d6ccd2
Sandhyapulicharla <sanjureddypulicharla1234@gmail.com>
2024-11-04 20:12:09 +0530

    Java_FullStack
    Project Added

---
+++ core-java-workspace-edubridge/coreJava/src/oct31/AreaOfFigures.java
@@ -0,0 +1,28 @@
+package oct31;
+
+public class AreaOfFigures {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			//area of reactangle
+//			float l=9.4f;
+//			float b=8.3f;
+//			float area;
+//			
+//			area=l*b;
+//		System.out.println("Area of reactagle of length="+l+" and breadth="+b+" is "+area);
+		
+		int b=5;
+		int h=7;
+		float area;
+		// area =(float)1/2*b*h; //explicit casting
+		 //area =1f/2*b*h;
+		 area =0.5f*b*h;
+		 
+		 System.out.println("area of traingle of base="+b+" and height="+h+" is "+area);
+	
+
+		
+	}
+
+}
---
+++ core-java-workspace-edubridge/coreJava/src/oct31/HelloWorld.java
@@ -0,0 +1,10 @@
+package oct31;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("My name is Sandhya");
+		System.out.println("I am from Banglore");
+		
+	}
+
+}
